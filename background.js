b307e16315322b23b9a749ad05afd3c68cc7f6d5
can2 <279319529@qq.com>
2017-04-22 13:03:28 +0800

    抓取页面数据

---
+++ background.js
@@ -0,0 +1,30 @@
+﻿function getDomainFromUrl(url){
+	var host = "null";
+	if(typeof url == "undefined" || null == url)
+		url = window.location.href;
+	var regex = /.*\:\/\/([^\/]*).*/;
+	var match = url.match(regex);
+	if(typeof match != "undefined" && null != match)
+		host = match[1];
+	return host;
+}
+
+function checkForValidUrl(tabId, changeInfo, tab) {
+	
+	if(getDomainFromUrl(tab.url).toLowerCase()=="acm.hdu.edu.cn"){
+		chrome.pageAction.show(tabId);
+	}
+
+};
+
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
+var info = {}
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+	// if(message.type == 'progress'){
+	// 	info.title = message.payload.title
+	// 	info.count = message.payload.count
+	// 	info.complate = message.payload.complate
+	// }
+	console.log(message)
+})
---
+++ content_script.js
@@ -0,0 +1,60 @@
+﻿function Tianti(){
+  var data = []
+  this.getData = function(){
+    return data;
+  }
+  this.privatePointRule = function(attr){
+    // attr为每道题的颜色数组
+    // 个人分数
+    var point = [5,5,10,10,15,15,20,25]
+
+  }
+  this.setData = function(){
+    var self = this
+    $("table").find("tr").each(function(index){
+      if(index != 0){
+        // $(this).find("td").each(function(index){
+          var td = $(this).find("td")
+
+          data = [
+          ...data,
+          {
+            "rank": $(td[0]).text(),
+            "name": $(td[1]).text(),
+            "solved": $(td[2]).text(),
+            "penalty": $(td[3]).text(),
+            "1001": $(td[4]).text(),
+            "1002": $(td[5]).text(),
+            "1003": $(td[6]).text(),
+            "1004": $(td[7]).text(),
+            "1005": $(td[8]).text(),
+            "1006": $(td[9]).text(),
+            "1007": $(td[10]).text(),
+            "1008": $(td[11]).text(),
+            "point": self.privatePointRule([
+              $(td[4]).text(),
+              $(td[5]).text(),
+              $(td[6]).text(),
+              $(td[7]).text(),
+              $(td[8]).text(),
+              $(td[9]).text(),
+              $(td[10]).text(),
+              $(td[11]).text(),
+            ])
+          }]
+        // })
+      }
+    })
+    data = data.sort(function(pre, next){
+      return +(pre.name > next.name) || +(pre.name === next.name) - 1;
+    })
+    return this
+  }
+  // var f = new Tianti()
+  // return f
+}
+
+var f = new Tianti()
+$(document).ready(function () {
+  console.log(f.setData().getData())
+})
